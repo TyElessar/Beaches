9a982e71ebca085ec9e33fa9d1ccb9598b8ff29d
Tovarishch Alcazarovich <62358700+TyElessar@users.noreply.github.com>
2023-04-27 10:25:59 +0200

    Updated empty project

---
+++ Source/Beaches/Beaches.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Beaches.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Beaches, "Beaches" );
---
+++ Source/Beaches/BeachesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeachesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEACHES_API ABeachesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
